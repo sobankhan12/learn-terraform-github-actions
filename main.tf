9dd9d947342409394cfd518a2cfc79530631fc04
Soban khan <sobankhan252@gmail.com>
2022-04-03 11:47:01 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,59 +2,36 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      #version = "3.26.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      #version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
+  #required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "soban252"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "ws-ryfS9EH75tcKu6LJ"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-04505e74c0741db8d"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
 
 resource "aws_security_group" "web-sg" {
